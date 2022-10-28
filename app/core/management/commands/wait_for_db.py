acea3216521a2ea75e4ffaabcfbec4a24887a123
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2022-10-28 23:19:40 +0500

    Fixed wait_for_db to avoid race condition

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,26 @@
+"""
+Django command to wait for the database to be available.
+"""
+import time
+
+from psycopg2 import OperationalError as Psycog2Error
+
+from django.db.utils import OperationalError
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """Django command to wait for the database."""
+
+    def handle(self, *args, **options):
+        """Entry point for command."""
+        self.stdout.write('Waiting for the database....')
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except (Psycog2Error, OperationalError):
+                self.stdout.write('Database unavailable, waiting for 1 second...')
+                time.sleep(1)
+        self.stdout.write(self.style.SUCCESS('Database is ready.'))
