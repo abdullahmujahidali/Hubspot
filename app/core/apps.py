823b0614e5e7bef37bd017f72faa82231776b70d
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2022-10-29 17:52:01 +0500

    setup for deployment complete

--- app/core/apps.py
+++ app/core/apps.py
@@ -1,12 +1,11 @@
 from django.apps import AppConfig
 
-from core.cron import CronJob
-
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
 
     def ready(self):
-        job = CronJob()
+        from app import cron
+        job = cron.CronJob()
         job.start()
